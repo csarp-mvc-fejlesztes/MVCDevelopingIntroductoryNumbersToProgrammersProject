22232f48216ac8d4993a9c81359c84886bc7f1cd
Csaba Gyuris <gycsaba@vasvari.hu>
2019-05-08 11:27:49 +0200

    ProgrammersRepository

---
+++ NumbersToProgrammers/repository/ProgrammersRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProgrammersProjekt.modell;
+
+namespace ProgrammersProjekt.repository
+{
+    /// <summary>
+    /// Repository == adattár osztály
+    /// </summary>
+    class ProgrammersRepository
+    {
+        /// <summary>
+        /// Programozók listája
+        /// </summary>
+        private List<Programmer> programmers;
+
+        /// <summary>
+        /// Konstruktorban példányosítás
+        /// </summary>
+        public ProgrammersRepository()
+        {
+            programmers = new List<Programmer>();
+        }
+
+        /// <summary>
+        /// Getter
+        /// </summary>
+        /// <returns>Programozók listája</returns>
+        public List<Programmer> getProgrammers()
+        {
+            return programmers;
+        }
+    }
+}
