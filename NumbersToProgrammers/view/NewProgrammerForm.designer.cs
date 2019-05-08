ea6a1aca1c8f835b8351fcb5b693b6f0b1c1b3c4
Csaba Gyuris <gycsaba@vasvari.hu>
2019-05-08 10:46:06 +0200

    Kiinduló projekt

---
+++ NumbersToProgrammers/view/NewProgrammerForm.designer.cs
@@ -0,0 +1,271 @@
+﻿namespace ProgrammersProjekt.view
+{
+    partial class NewDataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxAdatok = new System.Windows.Forms.GroupBox();
+            this.buttonAddCity = new System.Windows.Forms.Button();
+            this.labelNev = new System.Windows.Forms.Label();
+            this.buttonDeteleDataFromControl = new System.Windows.Forms.Button();
+            this.labelNem = new System.Windows.Forms.Label();
+            this.radioButtonAge = new System.Windows.Forms.RadioButton();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.radioButtonMan = new System.Windows.Forms.RadioButton();
+            this.checkBoxGameProgrammer = new System.Windows.Forms.CheckBox();
+            this.labelHobi = new System.Windows.Forms.Label();
+            this.labelVaros = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.comboBoxCity = new System.Windows.Forms.ComboBox();
+            this.checkBoxWebProgrammer = new System.Windows.Forms.CheckBox();
+            this.checkBoxDesktopProgrammer = new System.Windows.Forms.CheckBox();
+            this.labelEletkor = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonMegseg = new System.Windows.Forms.Button();
+            this.groupBoxAdatok.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxAdatok
+            // 
+            this.groupBoxAdatok.Controls.Add(this.buttonAddCity);
+            this.groupBoxAdatok.Controls.Add(this.labelNev);
+            this.groupBoxAdatok.Controls.Add(this.buttonDeteleDataFromControl);
+            this.groupBoxAdatok.Controls.Add(this.labelNem);
+            this.groupBoxAdatok.Controls.Add(this.radioButtonAge);
+            this.groupBoxAdatok.Controls.Add(this.textBoxAge);
+            this.groupBoxAdatok.Controls.Add(this.radioButtonMan);
+            this.groupBoxAdatok.Controls.Add(this.checkBoxGameProgrammer);
+            this.groupBoxAdatok.Controls.Add(this.labelHobi);
+            this.groupBoxAdatok.Controls.Add(this.labelVaros);
+            this.groupBoxAdatok.Controls.Add(this.textBoxName);
+            this.groupBoxAdatok.Controls.Add(this.comboBoxCity);
+            this.groupBoxAdatok.Controls.Add(this.checkBoxWebProgrammer);
+            this.groupBoxAdatok.Controls.Add(this.checkBoxDesktopProgrammer);
+            this.groupBoxAdatok.Controls.Add(this.labelEletkor);
+            this.groupBoxAdatok.Location = new System.Drawing.Point(25, 12);
+            this.groupBoxAdatok.Name = "groupBoxAdatok";
+            this.groupBoxAdatok.Size = new System.Drawing.Size(251, 270);
+            this.groupBoxAdatok.TabIndex = 26;
+            this.groupBoxAdatok.TabStop = false;
+            this.groupBoxAdatok.Text = "Adatok";
+            // 
+            // buttonAddCity
+            // 
+            this.buttonAddCity.Location = new System.Drawing.Point(227, 92);
+            this.buttonAddCity.Name = "buttonAddCity";
+            this.buttonAddCity.Size = new System.Drawing.Size(17, 23);
+            this.buttonAddCity.TabIndex = 11;
+            this.buttonAddCity.Text = "+";
+            this.buttonAddCity.UseVisualStyleBackColor = true;
+            // 
+            // labelNev
+            // 
+            this.labelNev.AutoSize = true;
+            this.labelNev.Location = new System.Drawing.Point(6, 39);
+            this.labelNev.Name = "labelNev";
+            this.labelNev.Size = new System.Drawing.Size(27, 13);
+            this.labelNev.TabIndex = 0;
+            this.labelNev.Text = "Név";
+            // 
+            // buttonDeteleDataFromControl
+            // 
+            this.buttonDeteleDataFromControl.Location = new System.Drawing.Point(4, 231);
+            this.buttonDeteleDataFromControl.Name = "buttonDeteleDataFromControl";
+            this.buttonDeteleDataFromControl.Size = new System.Drawing.Size(225, 23);
+            this.buttonDeteleDataFromControl.TabIndex = 10;
+            this.buttonDeteleDataFromControl.Text = "Töröl adatokat";
+            this.buttonDeteleDataFromControl.UseVisualStyleBackColor = true;
+            // 
+            // labelNem
+            // 
+            this.labelNem.AutoSize = true;
+            this.labelNem.Location = new System.Drawing.Point(6, 119);
+            this.labelNem.Name = "labelNem";
+            this.labelNem.Size = new System.Drawing.Size(29, 13);
+            this.labelNem.TabIndex = 4;
+            this.labelNem.Text = "Nem";
+            // 
+            // radioButtonAge
+            // 
+            this.radioButtonAge.AutoSize = true;
+            this.radioButtonAge.Location = new System.Drawing.Point(151, 119);
+            this.radioButtonAge.Name = "radioButtonAge";
+            this.radioButtonAge.Size = new System.Drawing.Size(37, 17);
+            this.radioButtonAge.TabIndex = 6;
+            this.radioButtonAge.TabStop = true;
+            this.radioButtonAge.Text = "nő";
+            this.radioButtonAge.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(151, 66);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.ShortcutsEnabled = false;
+            this.textBoxAge.Size = new System.Drawing.Size(70, 20);
+            this.textBoxAge.TabIndex = 2;
+            // 
+            // radioButtonMan
+            // 
+            this.radioButtonMan.AutoSize = true;
+            this.radioButtonMan.Location = new System.Drawing.Point(79, 119);
+            this.radioButtonMan.Name = "radioButtonMan";
+            this.radioButtonMan.Size = new System.Drawing.Size(42, 17);
+            this.radioButtonMan.TabIndex = 5;
+            this.radioButtonMan.TabStop = true;
+            this.radioButtonMan.Text = "férfi";
+            this.radioButtonMan.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGameProgrammer
+            // 
+            this.checkBoxGameProgrammer.AutoSize = true;
+            this.checkBoxGameProgrammer.Location = new System.Drawing.Point(49, 207);
+            this.checkBoxGameProgrammer.Name = "checkBoxGameProgrammer";
+            this.checkBoxGameProgrammer.Size = new System.Drawing.Size(108, 17);
+            this.checkBoxGameProgrammer.TabIndex = 9;
+            this.checkBoxGameProgrammer.Text = "jatékprogram írás";
+            this.checkBoxGameProgrammer.UseVisualStyleBackColor = true;
+            // 
+            // labelHobi
+            // 
+            this.labelHobi.AutoSize = true;
+            this.labelHobi.Location = new System.Drawing.Point(7, 145);
+            this.labelHobi.Name = "labelHobi";
+            this.labelHobi.Size = new System.Drawing.Size(60, 13);
+            this.labelHobi.TabIndex = 17;
+            this.labelHobi.Text = "Szaktudás:";
+            // 
+            // labelVaros
+            // 
+            this.labelVaros.AutoSize = true;
+            this.labelVaros.Location = new System.Drawing.Point(6, 92);
+            this.labelVaros.Name = "labelVaros";
+            this.labelVaros.Size = new System.Drawing.Size(45, 13);
+            this.labelVaros.TabIndex = 10;
+            this.labelVaros.Text = "Helység";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(70, 40);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(152, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // comboBoxCity
+            // 
+            this.comboBoxCity.FormattingEnabled = true;
+            this.comboBoxCity.Location = new System.Drawing.Point(70, 92);
+            this.comboBoxCity.Name = "comboBoxCity";
+            this.comboBoxCity.Size = new System.Drawing.Size(151, 21);
+            this.comboBoxCity.TabIndex = 3;
+            // 
+            // checkBoxWebProgrammer
+            // 
+            this.checkBoxWebProgrammer.AutoSize = true;
+            this.checkBoxWebProgrammer.Location = new System.Drawing.Point(49, 184);
+            this.checkBoxWebProgrammer.Name = "checkBoxWebProgrammer";
+            this.checkBoxWebProgrammer.Size = new System.Drawing.Size(89, 17);
+            this.checkBoxWebProgrammer.TabIndex = 8;
+            this.checkBoxWebProgrammer.Text = "webfejlesztés";
+            this.checkBoxWebProgrammer.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDesktopProgrammer
+            // 
+            this.checkBoxDesktopProgrammer.AutoSize = true;
+            this.checkBoxDesktopProgrammer.Location = new System.Drawing.Point(49, 161);
+            this.checkBoxDesktopProgrammer.Name = "checkBoxDesktopProgrammer";
+            this.checkBoxDesktopProgrammer.Size = new System.Drawing.Size(86, 17);
+            this.checkBoxDesktopProgrammer.TabIndex = 7;
+            this.checkBoxDesktopProgrammer.Text = "programozás";
+            this.checkBoxDesktopProgrammer.UseVisualStyleBackColor = true;
+            // 
+            // labelEletkor
+            // 
+            this.labelEletkor.AutoSize = true;
+            this.labelEletkor.Location = new System.Drawing.Point(7, 66);
+            this.labelEletkor.Name = "labelEletkor";
+            this.labelEletkor.Size = new System.Drawing.Size(40, 13);
+            this.labelEletkor.TabIndex = 1;
+            this.labelEletkor.Text = "Életkor";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Location = new System.Drawing.Point(57, 315);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // buttonMegseg
+            // 
+            this.buttonMegseg.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonMegseg.Location = new System.Drawing.Point(161, 315);
+            this.buttonMegseg.Name = "buttonMegseg";
+            this.buttonMegseg.Size = new System.Drawing.Size(75, 23);
+            this.buttonMegseg.TabIndex = 1;
+            this.buttonMegseg.Text = "Mégsem";
+            this.buttonMegseg.UseVisualStyleBackColor = true;
+            // 
+            // NewDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 388);
+            this.Controls.Add(this.buttonMegseg);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.groupBoxAdatok);
+            this.Name = "NewDataForm";
+            this.Text = "Új adatfelvitel";
+            this.groupBoxAdatok.ResumeLayout(false);
+            this.groupBoxAdatok.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxAdatok;
+        private System.Windows.Forms.Button buttonAddCity;
+        private System.Windows.Forms.Label labelNev;
+        private System.Windows.Forms.Button buttonDeteleDataFromControl;
+        private System.Windows.Forms.Label labelNem;
+        private System.Windows.Forms.RadioButton radioButtonAge;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.RadioButton radioButtonMan;
+        private System.Windows.Forms.CheckBox checkBoxGameProgrammer;
+        private System.Windows.Forms.Label labelHobi;
+        private System.Windows.Forms.Label labelVaros;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ComboBox comboBoxCity;
+        private System.Windows.Forms.CheckBox checkBoxWebProgrammer;
+        private System.Windows.Forms.CheckBox checkBoxDesktopProgrammer;
+        private System.Windows.Forms.Label labelEletkor;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonMegseg;
+    }
+}
