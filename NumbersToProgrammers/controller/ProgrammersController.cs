d21c370b9bcfbe8b19cbcc86052ca74f53f0ff1a
Gyuris Csaba <gycsaba@vasvari.hu>
2019-05-13 19:53:40 +0200

    Kapcsolat a controller és a repository réteg között.

--- NumbersToProgrammers/controller/ProgrammersController.cs
+++ NumbersToProgrammers/controller/ProgrammersController.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ProgrammersProjekt.repository;
+
 namespace ProgrammersProjekt.controller
 {
+    /// <summary>
+    /// Feladata:
+    ///  - az adattárban lévő adatok elérése, kezelése
+    ///  - az outputra szánt adatok átalakítása a megfeleő formátumba
+    ///  - input adatok ellenörzése
+    ///  - hibaüzenetek megjelenítése a logba vagy a felhasználó felé (kivételdobás)
+    /// </summary>
     class ProgrammersController
     {
+        /// <summary>
+        /// A vezérlő kezeli a repositoryban (adattárban) lévő adatokat
+        /// (Elv: a felső réteg igénybe veszi az alsóbb rétegek szolgáltatásait
+        /// </summary>
+        private ProgrammersRepository programmerRepository;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public ProgrammersController()
+        {
+            //A repository réteget képviselő osztály példányosítása
+            programmerRepository = new ProgrammersRepository();
+        }
+
     }
 }
